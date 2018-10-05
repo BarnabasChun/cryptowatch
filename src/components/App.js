38afc9c20264be2112b47ba116a01838e07c481b
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-05 10:48:29 -0400

    Add App component

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html {
+  font-size: 62.5%;
+    box-sizing: border-box;
+  }
+
+  *, 
+  *::after, 
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  a { text-decoration: none; }
+
+  ul, li { list-style: none; }
+`;
+
+const App = () => (
+  <div>
+    <GlobalStyle />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={() => <div>Watchlist</div>} />
+        <Route path="/coins/:symbol/overview" component={() => <div>Coin Overview</div>} />
+      </Switch>
+    </Router>
+  </div>
+);
+
+export default App;

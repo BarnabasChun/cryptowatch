7ac926c2ac5965960ea95bf9c4c21cf56b834043
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-06 19:11:46 -0400

    Add CryptoChart component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Wrapper } from './utils';
 import OverviewCard from './OverviewCard';
+import CryptoChart from './CryptoChart';
 
 const Overview = ({ currency, ...props }) => (
   <Wrapper>
       <OverviewCard currency={currency} {...props} className="overview-card" />
+      <CryptoChart currency={currency} {...props} />
   </Wrapper>
 );
 

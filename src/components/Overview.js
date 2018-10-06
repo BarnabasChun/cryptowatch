b54b0687be1052e500cbdde591b3fee518538d85
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-06 19:11:59 -0400

    Add styles to Overview component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Wrapper } from './utils';
 import OverviewCard from './OverviewCard';
 import CryptoChart from './CryptoChart';
 
+const StyledOverview = styled.div`
+  padding: 40px 0;
+
+  .overview-card {
+    margin-bottom: 30px;
+  }
+`;
+
 const Overview = ({ currency, ...props }) => (
-  <Wrapper>
+  <StyledOverview>
+    <Wrapper>
       <OverviewCard currency={currency} {...props} className="overview-card" />
       <CryptoChart currency={currency} {...props} />
-  </Wrapper>
+    </Wrapper>
+  </StyledOverview>
 );
 
 export default Overview;

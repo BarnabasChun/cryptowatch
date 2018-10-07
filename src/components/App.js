fcac546f7d20c895385627b49200519137df8e2f
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-07 13:59:02 -0400

    Remove default watchlist state

--- src/components/App.js
+++ src/components/App.js
@@ -25,40 +25,7 @@ const GlobalStyle = createGlobalStyle`
 export default class App extends Component {
   state = {
     currency: 'CAD',
-    watchlist: [
-      {
-        CHANGE24HOUR: -67.21,
-        CHANGEDAY: -1.69,
-        CHANGEPCT24HOUR: -0.73,
-        CHANGEPCTDAY: -0.018,
-        FLAGS: '2',
-        FROMSYMBOL: 'BTC',
-        HIGH24HOUR: 9176.92,
-        HIGHDAY: 9216.97,
-        LASTMARKET: 'QuadrigaCX',
-        LASTTRADEID: '3670110',
-        LASTUPDATE: 1538930954,
-        LASTVOLUME: 0.24,
-        LASTVOLUMETO: 2197.48,
-        LOW24HOUR: 9072.69,
-        LOWDAY: 8143.76,
-        MARKET: 'CCCAGG',
-        MKTCAP: 157552459839.18,
-        OPEN24HOUR: 9169.1,
-        OPENDAY: 9103.58,
-        PRICE: 9101.89,
-        SUPPLY: 17309862,
-        TOSYMBOL: 'CAD',
-        TOTALVOLUME24H: 146864.015,
-        TOTALVOLUME24HTO: 1336724625.71,
-        TYPE: '5',
-        VOLUME24HOUR: 97.27,
-        VOLUME24HOURTO: 869880.37,
-        VOLUMEDAY: 57.7,
-        VOLUMEDAYTO: 521054.5,
-        name: 'Bitcoin (BTC)',
-      },
-    ],
+    watchlist: [],
   };
 
   updateWatchList = coinDetails => {

e29a2dda7e990a1355d801eb15b8fec5e3a32866
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-07 12:55:39 -0400

    Remove unused fn

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,3 @@
-const getNthWord = n => s => s.split(' ')[n - 1];
-
-export const getSecondWord = getNthWord(2);
-
 export const displayNumbersAfterDecimal = (n, digitsAfterZero = 2) => {
   const [beforeDecimal, afterDecimal] = n.toString().split('.');
   const indexOfFirstNonZero = afterDecimal

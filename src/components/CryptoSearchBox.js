8c04d8bc6fa71ab16b36f546bda11a2ceb93c6e7
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-11 11:48:05 -0400

    Move styles to parent Nav

--- src/components/CryptoSearchBox.js
+++ src/components/CryptoSearchBox.js
@@ -3,57 +3,11 @@ import withRouter from 'react-router-dom/withRouter';
 import Downshift from 'downshift';
 import matchSorter from 'match-sorter';
 import SearchIcon from '@material-ui/icons/Search';
-import { withStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
-import { fade } from '@material-ui/core/styles/colorManipulator';
 import last from 'lodash/last';
 import { getAllCoins } from '../api';
 import { removeParenthesis } from '../helpers';
-
-const searchInputStyles = theme => ({
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit,
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    // [theme.breakpoints.up('sm')]: {
-    //   width: 120,
-    //   '&:focus': {
-    //     width: 200,
-    //   },
-    // },
-  },
-});
 
 const SearchInput = ({ classes, inputProps, placeholder }) => (
   <div className={classes.search}>
@@ -72,8 +26,6 @@ const SearchInput = ({ classes, inputProps, placeholder }) => (
     />
   </div>
 );
-
-const SearchInputWithStyles = withStyles(searchInputStyles)(SearchInput);
 
 const SearchSuggestions = ({
   suggestion: { Id, FullName },
@@ -103,7 +55,7 @@ const SearchSuggestions = ({
   );
 };
 
-const CryptoSearchBox = ({ onChange, selectedCoin, coins, placeholder }) => (
+const CryptoSearchBox = ({ classes, onChange, selectedCoin, coins, placeholder }) => (
   <Downshift onChange={onChange} defaultHighlightedIndex={0} defaultSelectedItem={selectedCoin}>
     {({
       getInputProps, // Props to pass to the input
@@ -119,7 +71,7 @@ const CryptoSearchBox = ({ onChange, selectedCoin, coins, placeholder }) => (
       }).slice(0, 5);
       return (
         <div>
-          <SearchInputWithStyles inputProps={getInputProps} placeholder={placeholder} />
+          <SearchInput inputProps={getInputProps} placeholder={placeholder} classes={classes} />
           {isOpen &&
             !!inputValue.length &&
             !!filteredItems.length && (
@@ -169,11 +121,12 @@ class CryptoSearchBoxContainer extends Component {
   };
 
   render() {
-    const { placeholder } = this.props;
+    const { placeholder, classes } = this.props;
     const { coins, selectedCoin } = this.state;
     if (coins) {
       return (
         <CryptoSearchBox
+          classes={classes}
           coins={coins}
           selectedCoin={selectedCoin}
           onChange={this.handleChange}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,21 +2,61 @@ import React from 'react';
 import styled from 'styled-components';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import { withStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import CryptoSearchBox from './CryptoSearchBox';
 import { Wrapper } from './utils';
 import Logo from './Logo';
 
 const NavWrapper = styled(Wrapper)``;
 
-const Nav = () => (
+const navStyles = theme => ({
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+});
+
+const Nav = ({ classes }) => (
   <AppBar position="static">
     <Toolbar>
       <NavWrapper>
         <Logo />
-        <CryptoSearchBox placeholder="Search for cryptocurrencies" />
+        <CryptoSearchBox placeholder="Search for cryptocurrencies" classes={classes} />
       </NavWrapper>
     </Toolbar>
   </AppBar>
 );
 
-export default Nav;
+export default withStyles(navStyles)(Nav);

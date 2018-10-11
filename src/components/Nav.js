3aaf2c2e3ffa501452cc4d4da555614482da4ded
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-11 11:43:03 -0400

    Use material-ui components for Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import CryptoSearchBox from './CryptoSearchBox';
 import { Wrapper } from './utils';
+import Logo from './Logo';
 
 const NavWrapper = styled(Wrapper)``;
 
 const Nav = () => (
-  <nav>
-    <NavWrapper>
-      <CryptoSearchBox placeholder="Search for cryptocurrencies" />
-    </NavWrapper>
-  </nav>
+  <AppBar position="static">
+    <Toolbar>
+      <NavWrapper>
+        <Logo />
+        <CryptoSearchBox placeholder="Search for cryptocurrencies" />
+      </NavWrapper>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Nav;

faa53a6c49d59c9fbf98ceb8bc4f6aba20bcc30f
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-11 11:42:51 -0400

    Add Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'react-router-dom/Link';
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+
+const LogoLink = styled(Link)`
+  color: inherit;
+  text-transform: uppercase;
+  text-decoration: none;
+`;
+
+const Logo = () => (
+  <LogoLink to="/">
+    <Typography variant="headline" color="inherit">
+      Cryptowatch
+    </Typography>
+  </LogoLink>
+);
+
+export default Logo;

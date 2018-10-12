e28f8fd273ab472f20f1082a392b90933da2c492
Barnabas Chun <chun.barnabas@gmail.com>
2018-10-12 14:41:58 -0400

    Move GlobalStyle to utils

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,9 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
+import GlobalStyle from './utils';
 import Nav from './Nav';
 import Watchlist from './Watchlist';
 import Coins from './Coins';
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-  }
-
-  *, 
-  *::after, 
-  *::before {
-    margin: 0;
-    padding: 0;
-    box-sizing: inherit;
-  }
-
-  a { text-decoration: none; }
-
-  ul, li { list-style: none; }
-`;
 
 export default class App extends Component {
   state = {
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,7 +1,27 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, 
+  *::after, 
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  a { text-decoration: none; }
+
+  ul, li { list-style: none; }
+`;
 
 export const Wrapper = styled.div`
   max-width: 1170px;
   width: 90%;
   margin: 0 auto;
 `;
+
+export default GlobalStyle;
